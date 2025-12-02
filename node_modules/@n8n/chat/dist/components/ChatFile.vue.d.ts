7a55380a6495bda726f172a719d9434fc25cf3c3
Takalani Sandani <takiesandani@gmail.com>
2025-12-02 23:07:39 +0200

    Added

---
+++ node_modules/@n8n/chat/dist/components/ChatFile.vue.d.ts
@@ -0,0 +1,12 @@
+type __VLS_Props = {
+    file: File;
+    isRemovable: boolean;
+    isPreviewable?: boolean;
+    href?: string;
+};
+declare const _default: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+    remove: (value: File) => any;
+}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{
+    onRemove?: ((value: File) => any) | undefined;
+}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, HTMLDivElement>;
+export default _default;
